70bf3b13f8db4ae300a192bf66f93803d3e0a1a1
sisiea <644022748@qq.com>
2017-07-20 21:31:31 +0800

    first

---
+++ index.js
@@ -0,0 +1,46 @@
+// 初始化游戏history
+var hasLucky = localStorage.getItem('sisiea_lottery_has');
+var gameTime = localStorage.getItem('sisiea_lottery_time');
+
+if (!hasLucky) {
+    localStorage.setItem('sisiea_lottery_has', 0);
+}
+else {
+    hasLucky = parseInt(hasLucky);
+}
+if (!gameTime) {
+    gameTime = 0;
+    localStorage.setItem('sisiea_lottery_time', gameTime);
+}
+else {
+    gameTime = parseInt(gameTime);
+}
+
+//内地结果
+
+var preResult = ['images/scratch_no.png','images/scratch_no.png','images/card_awards.png'];
+
+var overTime = 'images/overtime.png';
+
+
+
+var img = new Image()
+img.src = 'images/placeholder.jpg'
+img.onload = function () {
+    var lottery = new LotteryCard(document.getElementById('js_lottery'), { // eslint-disable-line
+        cover: img
+    })
+    lottery.on('start', function () {
+        if(gameTime>2){
+            lottery.setResult(overTime);
+        }else{
+            lottery.setResult(preResult[gameTime]);
+        }
+        gameTime++;
+        localStorage.setItem('sisiea_lottery_time', gameTime);
+        
+    })
+    lottery.on('end', function () {
+    })
+    window.lottery = lottery
+}
